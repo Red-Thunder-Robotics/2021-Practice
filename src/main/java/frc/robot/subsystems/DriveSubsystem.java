4216345e8503c5e5dd0e29e01aefe3bc02c0461a
Paul Garrett <paul.garrett@laingsburg.k12.mi.us>
2020-10-22 13:18:09 -0400

    Day 1

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveSubsystem extends SubsystemBase {
+  /**
+   * Creates a new DriveSubsystem.
+   */
+
+   public CANSparkMax frontLeft = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_CAN, MotorType.kBrushless);
+   public CANSparkMax frontRight = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_CAN, MotorType.kBrushless);
+   public CANSparkMax backLeft = new CANSparkMax(Constants.BACK_LEFT_MOTOR_CAN, MotorType.kBrushless);
+   public CANSparkMax backRight = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_CAN, MotorType.kBrushless);
+   
+   // Group motors
+
+   public SpeedControllerGroup leftGroup = new SpeedControllerGroup(frontLeft, backLeft);
+   public SpeedControllerGroup rightGroup = new SpeedControllerGroup(frontRight, backRight);
+   
+   // Defining Differential Drive
+
+   public DifferentialDrive differentialRocketLeagueDrive = new DifferentialDrive(leftGroup, rightGroup);
+
+
+  public void RocketLeagueDrive(double speed, double turn){
+    differentialRocketLeagueDrive.arcadeDrive(speed, turn);
+
+  }
+
+  public DriveSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
